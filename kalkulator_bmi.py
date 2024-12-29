f8721c81dd65e9672ca167a5dc3a603fb412b451
Guna Januarta <gunajanuarta6@gmail.com>
2024-12-29 23:27:57 +0800

    Penambahan Perkondisian

--- kalkulator_bmi.py
+++ kalkulator_bmi.py
@@ -13,8 +13,20 @@ berat_badan_ideal = dict()
 berat_badan_ideal['bawah'] = 18.5*(tinggi_badan**2)
 berat_badan_ideal['atas'] = 25*(tinggi_badan**2)
 
-print(f"\nNilai BMI anda adalah {bmi:.2f} kg/m^2")
-print("Nilai BNI normal berada dalam rentang 18.5 kg/m^2 - 25 kg/m^2")
+if bmi <= 18.5:
+    kategori = "Anda Kekurangan Berat Badan"
+elif bmi <= 25:
+    kategori = "Nilai BMI Anda Normal"
+elif bmi <= 30:
+    kategori = "Anda Kelebihan Berat Badan"
+else :
+    kategori = "Anda Mengalami Obesitas"
+
+print("\n----------------------------------")
+print("Hasil Kalkulasi BNI Anda Adalah : ")
+print("----------------------------------")
+print(f"Nilai BMI Anda adalah {bmi:.2f} kg/m^2")
+print(kategori)
 print(f"Berat badan yang ideal untuk anda berada dalam rentang {berat_badan_ideal['bawah']:.2f} kg - {berat_badan_ideal['atas']:.2f} kg ")
 print("-------------------------------------------")
 print("Terima Kasih Sudah Menggunakan Program Ini")
